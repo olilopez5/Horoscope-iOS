b1ea087e9cd77fed6dfcb6d55ffccb44c7a15c70
olilopez5 <olilopez5@outlook.es>
2025-05-12 10:23:18 +0200

    Initial Commit

---
+++ Horoscope-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Horoscope-iOS
+//
+//  Created by Mañanas on 12/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
